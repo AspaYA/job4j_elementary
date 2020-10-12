c3620b2f0abd7242c5e7c8e70f571ac01551924b
aspa <kreoll@gmail.com>
2020-10-12 18:19:46 +0300

    other fix

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -15,5 +15,34 @@ public class Calculator {
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
+
+        int age = 18; /* создаем переменную. */
+        System.out.println(age);
+        age = 19; /* присваиваем новое значение. */
+        System.out.println(age);
+
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
+
+        Calculator.plus(100, 500);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 5);
+
+        String name = "Petr Arsentev";
+        Calculator.hello(name);
     }
+
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+
+
 }
