f1094133dafa84b139c4a4c490e0642e54c61fdf
aspa <kreoll@gmail.com>
2020-10-14 11:19:41 +0300

    5.4. Протеиновая диета [#387480]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int user1, int user2) {
+        int user1Factor = 3;
+        int user2Factor = 2;
+        int count = 0;
+        while (user1 < user2) {
+            count++;
+            user1 *= user1Factor;
+            user2 *= user2Factor;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(90, 95));
+    }
+}
---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+
+    @Test
+    public void whenCalcU195U290Then0() {
+        assertThat(Fitness.calc(95, 90), is(0));
+    }
+
+    @Test
+    public void whenCalcU190U295Then0() {
+        assertThat(Fitness.calc(90, 95), is(1));
+    }
+
+    @Test
+    public void whenCalcU150U290Then0() {
+        assertThat(Fitness.calc(50, 90), is(2));
+    }
+
+    @Test
+    public void whenCalcU190U290Then0() {
+        assertThat(Fitness.calc(95, 90), is(0));
+    }
+}
