a36893f27a03242707e6e6386fa639ee35584a3a
aspa <kreoll@gmail.com>
2020-10-14 15:08:17 +0300

    5.5. Простое число [#387483] 5.6. Простые числа [#387481]

--- src/u/job4j/loop/CheckPrimeNumber.java
+++ src/u/job4j/loop/CheckPrimeNumber.java
@@ -4,10 +4,20 @@ public class CheckPrimeNumber {
     public static boolean check(int n) {
         boolean res = (n > 1); // ? true : false;
         for (int i = 2; i < n; i++) {
-            if (n % 2 == 0) {
+            if (n % i == 0) {
                 res = false;
                 break;
             }
+        }
+        return res;
+    }
+    public static int calc(int n) {
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!check(i)) {
+                continue;
+            }
+            res++;
         }
         return res;
     }
--- test/u/job4j/loop/CheckPrimeNumberTest.java
+++ test/u/job4j/loop/CheckPrimeNumberTest.java
@@ -19,4 +19,29 @@ public class CheckPrimeNumberTest {
     public void whenCheck1ThenFalse() {
         assertThat(CheckPrimeNumber.check(1), is(false));
     }
+
+    @Test
+    public void whenCheck11ThenTrue() {
+        assertThat(CheckPrimeNumber.check(11), is(true));
+    }
+
+    @Test
+    public void whenCheck9ThenFalse() {
+        assertThat(CheckPrimeNumber.check(9), is(false));
+    }
+
+    @Test
+    public void whenCalcN5Then3() {
+        assertThat(CheckPrimeNumber.calc(5), is(3));
+    }
+
+    @Test
+    public void whenCalcN11Then5() {
+        assertThat(CheckPrimeNumber.calc(11), is(5));
+    }
+
+    @Test
+    public void whenCalcN2Then1() {
+        assertThat(CheckPrimeNumber.calc(2), is(1));
+    }
 }
