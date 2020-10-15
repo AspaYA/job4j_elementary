f3393399d1c120b4793fa96528e912281a659d71
aspa <kreoll@gmail.com>
2020-10-15 15:16:25 +0300

    fix 6.5.3. Сортировка выборкой. [#387398] https://job4j.ru/edu/task?taskId=0873958f751ce70201751ceeb7d203e0&action=task&number=387398

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -5,7 +5,7 @@ public class SortSelected {
         // -1 потому что последнее сравнение делать не нужно
         for (int i = 0; i < (arr.length - 1); i++) {
             int min = MinDiapason.findMin(arr, i, arr.length);
-            int index = FindLoop.indexOf(arr, min, 0, arr.length);
+            int index = FindLoop.indexOf(arr, min, i, arr.length);
             SwitchArray.swap(arr, i, index);
         }
         return arr;
