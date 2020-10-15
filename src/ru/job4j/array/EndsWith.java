6646273098ac60cf2971ba02ac070eafa96b322e
aspa <kreoll@gmail.com>
2020-10-15 10:14:41 +0300

    6.4.2. Слово заканчивается на ... [#387396] https://job4j.ru/edu/task?action=task&taskId=0873958f751ce70201751ceeb7b003de&number=387396

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class EndsWith {
+
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean rst = true;
+        if (post.length > word.length) {
+            return false;
+        }
+        for (int i = 0; i < post.length; i++) {
+            int wordFakeI = word.length - 1 - i;
+            int postFakeI = post.length - 1 - i;
+            if (post[postFakeI] != word[wordFakeI]) {
+                rst = false;
+                break;
+            }
+        }
+        return  rst;
+    }
+}
---
+++ test/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class EndsWithTest {
+
+    @Test
+    public void whenEndsWithTrue() {
+        char[] word = {'A', 'B', 'C', 'D'};
+        char[] post = {'C', 'D'};
+        assertThat(EndsWith.endsWith(word, post), is(true));
+    }
+
+    @Test
+    public void whenEndsWithFalse() {
+        char[] word = {'A', 'B', 'C', 'D'};
+        char[] post = {'A', 'C', 'D'};
+        assertThat(EndsWith.endsWith(word, post), is(false));
+    }
+
+    @Test
+    public void whenEndsWithBadArrFalse() {
+        char[] word = {'A', 'B', 'C', 'D'};
+        char[] post = {'A', 'A', 'B', 'C', 'D'};
+        assertThat(EndsWith.endsWith(word, post), is(false));
+    }
+}
